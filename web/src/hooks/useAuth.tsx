f316cc45f07aead6a1bbe2309f8f748f4f5cc4d7
Uriel de Oliveira Alves <uriel.oalves@gmail.com>
2022-02-26 23:55:46 +0100

    add toast and spinner for authentication

--- web/src/hooks/useAuth.tsx
+++ web/src/hooks/useAuth.tsx
@@ -1,10 +1,11 @@
-import React, { createContext, useCallback, useState, useContext } from 'react';
+import React, { createContext, useCallback, useState, useContext } from "react";
 import {
   GoogleLoginResponse,
   GoogleLoginResponseOffline,
-} from 'react-google-login';
+} from "react-google-login";
 
-import { api } from '../services';
+import { api } from "../services";
+import { useToast } from "./useToast";
 
 const GOOGLE_CLIENT_ID = process.env.REACT_APP_GOOGLE_CLIENT_ID as string;
 export interface User {
@@ -19,12 +20,13 @@ interface AuthState {
 interface AuthContextData {
   user: User;
   googleClientId: string;
+  isLoading: boolean;
   signOut(): void;
   onGoogleSignInSuccess(
-    res: GoogleLoginResponse | GoogleLoginResponseOffline,
+    res: GoogleLoginResponse | GoogleLoginResponseOffline
   ): void;
   onGoogleSignUpSuccess(
-    res: GoogleLoginResponse | GoogleLoginResponseOffline,
+    res: GoogleLoginResponse | GoogleLoginResponseOffline
   ): void;
 }
 
@@ -32,8 +34,8 @@ const AuthContext = createContext<AuthContextData>({} as AuthContextData);
 
 const AuthProvider: React.FC = ({ children }) => {
   const [data, setData] = useState<AuthState>(() => {
-    const token = localStorage.getItem('@weStudy:token');
-    const user = localStorage.getItem('@weStudy:user');
+    const token = localStorage.getItem("@weStudy:token");
+    const user = localStorage.getItem("@weStudy:user");
 
     if (token && user) {
       api.defaults.headers.common.authorization = token;
@@ -47,54 +49,86 @@ const AuthProvider: React.FC = ({ children }) => {
     return {} as AuthState;
   });
 
+  const [isLoading, setIsLoading] = useState(false);
+  const { showToast } = useToast();
+
   function onGoogleSignInSuccess(res: GoogleLoginResponse): void {
+    setIsLoading(true);
     api
-      .post('/secure/auth', {
+      .post("/secure/auth", {
         token: res.tokenId,
       })
-      .then(response => {
+      .then((response) => {
         const { token } = response.data;
         // @TODO: get user from response.data once it is implemented in the backend
         const user = {
-          id: 'fake_id',
-          name: 'fake_name',
+          id: "fake_id",
+          name: "fake_name",
         };
-        localStorage.setItem('@weStudy:token', token);
-        localStorage.setItem('@weStudy:user', JSON.stringify(user));
+        localStorage.setItem("@weStudy:token", token);
+        localStorage.setItem("@weStudy:user", JSON.stringify(user));
 
         api.defaults.headers.common.authorization = token;
 
         setData({ token, user });
+        setIsLoading(false);
+        showToast({
+          status: "success",
+          title: "Authentication",
+          description: "You are signed in",
+        });
       })
-      .catch(err => console.error(err));
+      .catch((err) => {
+        console.error(err);
+        setIsLoading(false);
+        showToast({
+          status: "success",
+          title: "Authentication",
+          description: "Could not sign in. Please try again.",
+        });
+      });
   }
 
   function onGoogleSignUpSuccess(res: GoogleLoginResponse): void {
+    setIsLoading(true);
     api
-      .post('/secure/signUp', {
+      .post("/secure/signUp", {
         token: res.tokenId,
       })
-      .then(response => {
+      .then((response) => {
         const { token } = response.data;
         // @TODO: get user from response.data once it is implemented in the backend
         const user = {
-          id: 'fake_id',
-          name: 'fake_name',
+          id: "fake_id",
+          name: "fake_name",
         };
 
-        localStorage.setItem('@weStudy:token', token);
-        localStorage.setItem('@weStudy:user', JSON.stringify(user));
+        localStorage.setItem("@weStudy:token", token);
+        localStorage.setItem("@weStudy:user", JSON.stringify(user));
 
         api.defaults.headers.common.authorization = token;
 
         setData({ token, user });
+        setIsLoading(false);
+        showToast({
+          status: "success",
+          title: "Authentication",
+          description: "You are signed in",
+        });
       })
-      .catch(err => console.error(err));
+      .catch((err) => {
+        setIsLoading(false);
+        showToast({
+          status: "error",
+          title: "Authentication",
+          description: "Could not sign up. Please try again.",
+        });
+      });
   }
 
   const signOut = useCallback(() => {
-    localStorage.removeItem('@weStudy:token');
-    localStorage.removeItem('@weStudy:user');
+    localStorage.removeItem("@weStudy:token");
+    localStorage.removeItem("@weStudy:user");
 
     setData({} as AuthState);
   }, []);
@@ -104,6 +138,7 @@ const AuthProvider: React.FC = ({ children }) => {
       value={{
         user: data.user,
         googleClientId: GOOGLE_CLIENT_ID,
+        isLoading,
         onGoogleSignInSuccess,
         onGoogleSignUpSuccess,
         signOut,
@@ -118,7 +153,7 @@ const useAuth = (): AuthContextData => {
   const context = useContext(AuthContext);
 
   if (!context) {
-    throw new Error('useAuth must be used within an AuthProvider');
+    throw new Error("useAuth must be used within an AuthProvider");
   }
 
   return context;
--- web/src/pages/SignIn/index.tsx
+++ web/src/pages/SignIn/index.tsx
@@ -1,10 +1,14 @@
-import { Flex, Stack, Text } from '@chakra-ui/react';
-import GoogleLogin from 'react-google-login';
-import { useAuth } from '../../hooks';
+import { Flex, Spinner, Stack, Text } from "@chakra-ui/react";
+import GoogleLogin from "react-google-login";
+import { useAuth } from "../../hooks";
 
 export function SignIn() {
-  const { googleClientId, onGoogleSignInSuccess, onGoogleSignUpSuccess } =
-    useAuth();
+  const {
+    googleClientId,
+    onGoogleSignInSuccess,
+    onGoogleSignUpSuccess,
+    isLoading,
+  } = useAuth();
 
   return (
     <Flex
@@ -12,37 +16,48 @@ export function SignIn() {
       bgColor="blue.900"
       color="white"
       height={{
-        base: 'calc(100vh - var(--headerHeightMobile))',
-        md: 'calc(100vh - var(--headerHeight))',
+        base: "calc(100vh - var(--headerHeightMobile))",
+        md: "calc(100vh - var(--headerHeight))",
       }}
       justify="center"
     >
-      <Stack
-        mt="100px"
-        bgColor="gray.300"
-        color="gray.500"
-        width="400px"
-        height="fit-content"
-        padding="40px"
-        filter="drop-shadow(0px 4px 20px rgba(0, 0, 0, 0.25))"
-        borderRadius="10px"
-        direction="column"
-        align="center"
-        spacing="16px"
-      >
-        <Text>Sign In with</Text>
-        <GoogleLogin
-          clientId={googleClientId}
-          onSuccess={onGoogleSignInSuccess}
-          buttonText="Google"
+      {isLoading ? (
+        <Spinner
+          thickness="4px"
+          speed="0.65s"
+          emptyColor="gray.200"
+          color="blue.300"
+          size="xl"
+          mt="100px"
         />
-        <Text>Or sign up with</Text>
-        <GoogleLogin
-          clientId={googleClientId}
-          onSuccess={onGoogleSignUpSuccess}
-          buttonText="Google"
-        />
-      </Stack>
+      ) : (
+        <Stack
+          mt="100px"
+          bgColor="gray.300"
+          color="gray.500"
+          width="400px"
+          height="fit-content"
+          padding="40px"
+          filter="drop-shadow(0px 4px 20px rgba(0, 0, 0, 0.25))"
+          borderRadius="10px"
+          direction="column"
+          align="center"
+          spacing="16px"
+        >
+          <Text>Sign In with</Text>
+          <GoogleLogin
+            clientId={googleClientId}
+            onSuccess={onGoogleSignInSuccess}
+            buttonText="Google"
+          />
+          <Text>Or sign up with</Text>
+          <GoogleLogin
+            clientId={googleClientId}
+            onSuccess={onGoogleSignUpSuccess}
+            buttonText="Google"
+          />
+        </Stack>
+      )}
     </Flex>
   );
 }
